27a1eec570595bb253618301781fbff0aaebdd1e
robert <robert@alliedworld.healthcare>
2020-01-22 07:36:51 +0800

    test server

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,1 @@
-const chai = require("chai");
-const request = require("supertest");
-const http = require("chai-http");
-const server = require("../server");
-const config = require("config");
-chai.use(http);
-const expect = chai.expect;
+require("./server.test");
---
+++ test/server.test.js
@@ -0,0 +1,16 @@
+const chai = require("chai");
+const request = require("supertest");
+const http = require("chai-http");
+const server = require("../server");
+const config = require("config");
+
+const expect = chai.expect;
+
+describe("Server", () => {
+  it("Should exist", () => {
+    expect(server).to.be.a("function");
+  });
+  it("Should have running port", async () => {
+    expect(server.port).to.equal(config.get("PORT"));
+  });
+});
